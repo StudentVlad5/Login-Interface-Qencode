35303d00f1673b28d4a0e0216bda49892d14cf62
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-03-06 09:52:43 +0200

    add message service

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,79 +1,109 @@
-import axios from 'axios';
-import { BASE_URL } from './constant';
+import axios from "axios";
+import { BASE_URL } from "./constant";
+import { onFetchError, onSuccess } from "./Messages/NotifyMessages";
 
-const setAuthHeader = token => {
+const setAuthHeader = (token) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
 const clearAuthHeader = () => {
-  axios.defaults.headers.common.Authorization = '';
+  axios.defaults.headers.common.Authorization = "";
 };
 
-export const signUp = async credentials => {
+export const signUp = async (credentials) => {
   try {
     const res = await axios.post(`${BASE_URL}/auth/signup`, credentials);
-    if(res){setAuthHeader(res.access_token)};
+    if (res) {
+      setAuthHeader(res.access_token);
+    }
+    onSuccess("Success");
     return res;
   } catch (error) {
+    onFetchError(`Error: ${error.message}`);
     return error.message;
   }
 };
 
-export const LogIn = async credentials => {
+export const LogIn = async (credentials) => {
   try {
     const res = await axios.post(`${BASE_URL}/auth/login`, credentials);
-    if(res){setAuthHeader(res.access_token)};
+    if (res) {
+      setAuthHeader(res.access_token);
+    }
+    onSuccess("Success");
     return res;
   } catch (error) {
+    onFetchError(`Error: ${error.message}`);
     return error.message;
   }
 };
 
-export const changePassword = async credentials => {
+export const changePassword = async (credentials) => {
   try {
     const res = await axios.post(`${BASE_URL}/auth/password-set`, credentials);
-    if(res){setAuthHeader(res.access_token)};
+    if (res) {
+      setAuthHeader(res.access_token);
+    }
+    onSuccess("Success");
     return res;
   } catch (error) {
+    onFetchError(`Error: ${error.message}`);
     return error.message;
   }
 };
 
-export const forgotPassword = async credentials => {
+export const forgotPassword = async (credentials) => {
   try {
-    const res = await axios.post(`${BASE_URL}/auth/password-reset`, credentials);
+    const res = await axios.post(
+      `${BASE_URL}/auth/password-reset`,
+      credentials
+    );
+    onSuccess("Success");
     return res;
   } catch (error) {
+    onFetchError(`Error: ${error.message}`);
     return error.message;
   }
 };
 
 export const LogOut = async () => {
-  try{
-  const res = await axios.post(`${BASE_URL}/auth/logout`);
-  if(res){clearAuthHeader()};
-  return res;
+  try {
+    const res = await axios.post(`${BASE_URL}/auth/logout`);
+    if (res) {
+      clearAuthHeader();
+    }
+    onSuccess("Success");
+    return res;
   } catch (error) {
+    onFetchError(`Error: ${error.message}`);
     return error.message;
   }
 };
 
 export const accessToken = async () => {
-  try{
-  const res = await axios.post(`${BASE_URL}/auth/access-token`);
-  if(res){setAuthHeader(res.access_token)};
-  return res;
+  try {
+    const res = await axios.post(`${BASE_URL}/auth/access-token`);
+    if (res) {
+      setAuthHeader(res.access_token);
+    }
+    onSuccess("Success");
+    return res;
   } catch (error) {
-  return error.message;
+    onFetchError(`Error: ${error.message}`);
+    return error.message;
   }
 };
 
 export const refreshToken = async () => {
-  try{
-  const res = await axios.post(`${BASE_URL}/auth/refresh-token`);
-  if(res){setAuthHeader(res.access_token)};
-  return res;
-} catch (error) {
-  return error.message;
+  try {
+    const res = await axios.post(`${BASE_URL}/auth/refresh-token`);
+    if (res) {
+      setAuthHeader(res.access_token);
+    }
+    onSuccess("Success");
+    return res;
+  } catch (error) {
+    onFetchError(`Error: ${error.message}`);
+    return error.message;
   }
 };
